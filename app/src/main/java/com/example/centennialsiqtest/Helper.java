72261c6f6cf4ee87c35e80c279856a50357f86b4
gurjap <gurjaph@my.yorku.ca>
2022-04-02 17:01:25 -0400

    Added stock values

--- app/src/main/java/com/example/centennialsiqtest/Helper.java
+++ app/src/main/java/com/example/centennialsiqtest/Helper.java
@@ -1,17 +1,23 @@
 package com.example.centennialsiqtest;
 
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.ImageView;
 
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.annotation.Documented;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Random;
 
 import android.view.View;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Helper {
 
@@ -114,43 +120,16 @@ public class Helper {
         numbers();
     }
 
-    public static double getstock(String link) throws IOException {
-        URL url = new URL(link);
-        URLConnection urlConn = url.openConnection();
-        InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
-        BufferedReader buff = new BufferedReader(inStream);
+    public static String StringToStock(String line){
+        int ind = line.indexOf("After Hours");
+        Log.d("mylog",line);
+        String sub = line.substring(ind-10,ind);
 
-        String line = buff.readLine();
+        int index = sub.indexOf("$");
 
-        while (line != null) {
-            if (line.contains("data-last-price")) {
-                int target = line.indexOf("data-last-price");
-                int deci = line.indexOf(".", target);
-                int start = deci;
-                while (line.charAt(start) != '\"') {
-                    start--;
-                }
-                String price = line.substring(start + 1, deci + 1);
-                double pri = Double.parseDouble(price);
-                return pri;
-            }
-            line = buff.readLine();
-        }
-        return -1;
-    }
+        String result = sub.substring(index);
 
-    public static String stockinfo() throws IOException {
-        double apple = getstock("https://www.google.com/finance/quote/AAPL:NASDAQ?hl=en");
-        double tesla = getstock("https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en");
-        double micro = getstock("https://www.google.com/finance/quote/MSFT:NASDAQ?hl=en");
-        double netflix = getstock("https://www.google.com/finance/quote/NFLX:NASDAQ?hl=en");
-        double amazon = getstock("https://www.google.com/finance/quote/AMZN:NASDAQ?hl=en");
-        double alphabet = getstock("https://www.google.com/finance/quote/GOOGL:NASDAQ?hl=en");
-        double TD = getstock("https://www.google.com/finance/quote/TD:TSE?hl=en");
-
-        String ans = "Apple:\t"+apple+"\n"+"tesla:\t"+tesla+"\n"+"Microsoft:\t"+micro+"\n"+"Netflix:\t"+netflix+"\n"+"Amazon:\t"+amazon+"\n"+"Alphabet:\t"+alphabet+"\n"+"TD:\t"+TD;
-
-        return ans;
+        return result;
     }
 
     public static void pattern(ImageView b0, ImageView b1, ImageView b2, ImageView b3) {
